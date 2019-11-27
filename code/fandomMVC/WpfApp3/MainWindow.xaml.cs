d084aae0af5c842441e83f56a49c7e2ebd0aa328
Morten <mort381a@hotmail.com>
2019-11-27 01:45:42 +0100

    severice insert

---
+++ code/fandomMVC/WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,141 @@
+﻿using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ServiceHost _hostProductServices = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //inserted
+        // Configure the message box to be displayed
+        string messageBoxText = "Do you want to delete the product?";
+        string caption = "Word Processor";
+        MessageBoxButton button = MessageBoxButton.YesNo;
+        MessageBoxImage icon = MessageBoxImage.Warning;
+        //inserted close
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void TabControl_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            myTabControl.SelectedItem = editProductTab;
+        }
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            myTabControl.SelectedItem = productMenuTab;
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            myTabControl.SelectedItem = createProductTab;
+
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            myTabControl.SelectedItem = searchProductTab;
+
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            myTabControl.SelectedItem = productMenuTab;
+
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            myTabControl.SelectedItem = productMenuTab;
+
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+            myTabControl.SelectedItem = mainMenuTab;
+
+        }
+
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(messageBoxText, caption, button, icon);
+        }
+
+        private void Button_Click_10(object sender, RoutedEventArgs e)
+        {
+            myTabControl.SelectedItem = searchProductTab;
+        }
+
+        private void Button_Click_9(object sender, RoutedEventArgs e)
+        {
+            myTabControl.SelectedItem = searchProductTab;
+
+        }
+
+        private void Button_Click_11(object sender, RoutedEventArgs e)
+        {
+            _hostProductServices = new ServiceHost(typeof(ProductServices));
+            _hostProductServices.Open();
+
+            //do sometihing
+
+           
+
+        }
+
+        private void Button_Click_12(object sender, RoutedEventArgs e)
+        {
+            myTabControl.SelectedItem = productMenuTab;
+        }
+
+        private void Button_Click_13(object sender, RoutedEventArgs e)
+        {
+            myTabControl.SelectedItem = invetoryListTab;
+        }
+
+        private void Button_Click_14(object sender, RoutedEventArgs e)
+        {
+            myTabControl.SelectedItem = productMenuTab;
+        }
+    }
+}
