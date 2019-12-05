ce14f566964955766963bad004a0d1d36efeb2b0
Morten <mort381a@hotmail.com>
2019-12-05 21:29:10 +0100

    nothing i am adumbass2
    wesrwer

---
+++ code/fandomMVC/Contracts/IProductService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Contracts
+{
+        [ServiceContract]
+        public interface IProductService
+    {
+        [OperationContract]
+        ProductData GetProductByID(int ID);
+        [OperationContract]
+        void Insertproduct(ProductData productData);
+        [OperationContract]
+        void RemoveProduct(ProductData productData);
+        [OperationContract]
+        void updateProduct(ProductData productData);
+        [OperationContract]
+        List<ProductData> GetProducts();
+        [OperationContract]
+        ProductData GetProductByName(string Name);
+        [OperationContract]
+        void setViasableTofalse(ProductData productData);
+        [OperationContract]
+        void setViasableTotrue(ProductData productData);
+        [OperationContract]
+        IEnumerable<ProductData> GetlikeProdctNames(string name);
+    }
+}
---
+++ code/fandomMVC/DBlayerrr/IProductDB.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBlayerrr
+{
+        public interface IProductDB
+    {
+        void insertProduct(Product Product);
+        Product getProductByID(int id);
+        IEnumerable<Product> GetProducts();
+
+        void removeProduct(Product product);
+
+        void UpdateProduct(Product product);
+
+        Product GetProductByName(string name);
+
+        void productToUnviasable(Product product);
+
+        void productToviasble(Product product);
+
+        IEnumerable<Product> GetlikeProdctNames(string name);
+    }
+}
